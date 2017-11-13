c022a20601cf6e78074c493792f46509ef1d9201
Daniela Socas Gil <danielasocasgil@gmail.com>
2017-11-13 13:32:46 +0000

    base template html

--- ecg/views.py
+++ ecg/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from models import Record 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ECG WEB")
+    template = loader.get_template('ecg/index.html')
+    
+    return HttpResponse(template.render(request))
